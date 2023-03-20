6a203ecf20602f25635e7b87eca13d7bf0844330
Dennis Saji <dennissaji2002@gmail.com>
2023-03-20 19:01:54 +0530

    home page

--- home.php
+++ home.php
@@ -1,6 +1,54 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+ <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+ <link href="style.css" rel="stylesheet">
+ <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+   </head>
+   <body>
+     <input type="checkbox" id="check">
+     <label for="check">
+       <i class="fas fa-bars" id="btn"></i>
+       <i class="fas fa-times" id="cancel"></i>
+     </label>
+     <div class="sidebar">
+       <header>Menu</header>
+       <a href="#" class="active">
+        <i class="fas fa-home"></i>
+         <span>Home</span>
+       </a>
+       <a href="#">
+        <i class="fa fa-list-alt" aria-hidden="true"></i>
+         <span>Categories</span>
+       </a>
+       <a href="#">
+         <i class="fas fa-stream"></i>
+         <span>Items</span>
+       </a>
+       <a href="#">
+        <i class="fas  fa-user"></i>
+         <span>users</span>
+       </a>
+       <a href="#">
+         <i class="far fa-question-circle"></i>
+         <span>Report Issue</span>
+       </a>
+       <a href="#">
+         <i class="fas fa-sliders-h"></i>
+         <span>Services</span>
+       </a>
+       <a href="#">
+         <i class="far fa-envelope"></i>
+         <span>Contact</span>
+       </a>
+     </div>
+ 
+     
+   <div class="frame">
 
-echo "welcome to home"
-
-
-?>
+ 
+ </div>  
+     
+ </body>
+ </html>
